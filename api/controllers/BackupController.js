fc5b0bc1e170a8ca127718151f23e4907c51d9cd
André Missaglia <andre.missaglia@gmail.com>
2016-05-31 03:05:56 -0300

    Começa a implementar import/export do BD.

---
+++ api/controllers/BackupController.js
@@ -0,0 +1,54 @@
+module.exports = {
+	export: function(req, res){
+		output = {}
+		var next;
+		var i = 0;
+		var pipeline = [
+			//users
+			function(){
+				User.find().exec(function(err,users){
+					var list = [];
+					for (var i = 0; i < users.length; i++) {
+						list.push({
+							id:users[i].id,
+							nome:users[i].nome,
+							login:users[i].login,
+							password:users[i].password,
+							birthday:users[i].nascimento,
+							bio:users[i].descricao
+						});
+					}
+					output.users = list;
+					next();
+				})
+			}, 
+			//tweets
+			function(){
+				Tweet.find().exec(function(err,tweets){
+					var list = [];
+					for (var i = 0; i < tweets.length; i++) {
+						list.push({
+							id:tweets[i].id,
+							user:tweets[i].user,
+							title:tweets[i].title,
+							text:tweets[i].text,
+							timestamp:tweets[i].timestamp
+						});
+					}
+					output.tweets = list;
+					next();
+				})
+			}, 
+			//output
+			function(){
+				res.json(output);
+			}
+		];
+		next = function(){
+			if(i < pipeline.length){
+				pipeline[i++]();
+			}
+		};
+		next();
+	}
+}
